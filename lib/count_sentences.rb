bbf170ee0d2b241d5a19e19f53ba2ffff3a6a76e
Jasterix C <loune.calixte@gmail.com>
2019-04-28 00:43:17 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,24 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
 
+     x=self.split(/\.|\?|\!/)
+     binding.pry
+    x=x.delete_if {|word| word.size<2}
+
+    return x.size
   end
+  puts "done"
 end
